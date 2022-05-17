bae14c6a1f6573aa2fba6b3ff830b761616ce582
moralesja <aalberto_morales@hotmail.com>
2022-05-17 05:06:26 -0500

    update

--- 1-SCADA.py
+++ 1-SCADA.py
@@ -6,36 +6,6 @@ import os
 import sys
 pyautogui.FAILSAFE = True
 
-#def My_Documents(location):
-#	import ctypes.wintypes222
-#		#####-----This section discovers My Documents default path --------
-#	CSIDL_PERSONAL = location       # My Documents
-#	SHGFP_TYPE_CURRENT = 0   # Get current, not default value
-#	buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
-#	ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
-#	#####-------- please use buf.value to store the data in a variable ------- #####
-#	#add the text filename at the end of the path
-#	temp_docs = buf.value
-#	return temp_docs
-#
-#try to detect IE installation
-#try #1: x86 files
-#output
-#mis_docs1 = My_Documents(42)
-#ie_address86 = str(mis_docs1) + r"\Internet Explorer\iexplore.exe"
-#try2: 64bit 
-#mis_docs2 = My_Documents(38)
-#ie_address64 = str(mis_docs2) + r"\Internet Explorer\iexplore.exe"
-	#test if any of this exists
-#if os.path.exists(ie_address86):
-#	ie_address = ie_address86 
-#	subprocess.run([ie_address])
-#elif os.path.exists(ie_address64):
-#	ie_address = ie_address64
-#	subprocess.run([ie_address])
-#else:
-#	#show a message that there's no VLC installed
-#	tkinter.messagebox.showwarning(title="Failed Action", message= f"Internet Explorer not detected")
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
